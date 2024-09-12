23eb29262669ec62a06592fa63d2386cbbda00eb
Likith_devops <151631786+Likhi-sf@users.noreply.github.com>
2024-09-12 19:13:03 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "Prod-server" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    private_key = file("./practice Key.pem")
+    private_key = file("C:\Users\Likith C\OneDrive\Documents\StarAgile\practice Key.pem")
     host = self.public_ip
   }
   provisioner "remote-exec"{
