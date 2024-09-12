43add862b29ffc8163bc2234d8474706fa630954
Likith_devops <151631786+Likhi-sf@users.noreply.github.com>
2024-09-12 16:02:00 +0530

    Create main.tf

---
+++ terraform-files/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "Prod-server" {
+  ami           = "ami-0e86e20dae9224db8"  
+  instance_type = "t2.micro"
+  key_name = "practice Key"
+  security_groups = [
+      "sg-00bd58231a14bfc2c"
+  ]
+  tags = {
+    Name = "Prod-server"
+  }
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("./practice Key.pem")
+    host = self.public_ip
+  }
+  provisioner "remote-exec"{
+  inline ["echo 'wait to start the instance' "]
+  } 
+  provisioner "local-exec" {
+  command = "echo ${aws_instance.Prod-server.public_ip} > inventory"
+  }
+}
+  
+
+  
